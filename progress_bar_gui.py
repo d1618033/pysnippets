3f143fe86476377d1322e160f6030f566c8b8828
d1618033 <d1618033@gmail.com>
2015-05-09 17:35:11 +0300

    move progressbar + variable into class

--- progress_bar_gui.py
+++ progress_bar_gui.py
@@ -18,6 +18,19 @@ def command(progress_func):
         time.sleep(0.01)
 
 
+class Progressbar(ttk.Progressbar):
+    def __init__(self, *args, **kwargs):
+        self.progress_bar_value = tkinter.IntVar(value=0)
+        kwargs['variable'] = self.progress_bar_value
+        ttk.Progressbar.__init__(self, *args, **kwargs)
+
+    def set(self, value):
+        self.progress_bar_value.set(value)
+
+    def get(self):
+        return self.progress_bar_value.get()
+
+
 class App(ttk.Frame):
     def __init__(self, master, root):
         ttk.Frame.__init__(self, master=master)
@@ -31,8 +44,7 @@ class App(ttk.Frame):
             command=self.on_click
         )
         self.button.pack()
-        self.progress_bar_value = tkinter.IntVar(value=0)
-        self.progress_bar = ttk.Progressbar(self, variable=self.progress_bar_value)
+        self.progress_bar = Progressbar(self)
         self.progress_bar.pack()
 
     def disable_button(self):
@@ -54,7 +66,7 @@ class App(ttk.Frame):
         else:
             while not queue.empty():
                 p = queue.get()
-                self.progress_bar_value.set(p)
+                self.progress_bar.set(p)
             root.after(10, self.poll_progress, queue, process)
 
 
