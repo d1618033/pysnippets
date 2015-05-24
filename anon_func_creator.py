f9d658b55518b7239d7c985c113deb708b0f0539
d1618033 <d1618033@gmail.com>
2015-05-24 13:37:04 +0300

    compile string

--- anon_func_creator.py
+++ anon_func_creator.py
@@ -8,9 +8,10 @@ def create_func(string):
     calframe = inspect.getouterframes(curframe, 2)
     caller_locals = calframe[1][0].f_locals
     caller_globals = calframe[1][0].f_globals
+    compiled_string = compile(string, '', 'eval')
     if len(matches) == 0:
         def func():
-            return eval(string, caller_globals, caller_locals)
+            return eval(compiled_string, caller_globals, caller_locals)
     else:
         num_args = max(map(lambda x: int(x) if x else 1, map(lambda x: x.replace('_', ''), matches)))
 
@@ -21,6 +22,6 @@ def create_func(string):
             new_locals['_'] = args[0]
             for i, arg in enumerate(args):
                 new_locals['_{0}'.format(i+1)] = arg
-            return eval(string, caller_globals, new_locals)
+            return eval(compiled_string, caller_globals, new_locals)
     func.__name__ = string
     return func
