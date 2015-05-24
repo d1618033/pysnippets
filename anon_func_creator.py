bdb1f645d1aee15b95c6e5e925bcbc5604a8bf9d
d1618033 <d1618033@gmail.com>
2015-05-24 12:56:06 +0300

    add scope

--- anon_func_creator.py
+++ anon_func_creator.py
@@ -1,9 +1,13 @@
 import re
+import inspect
 
 
 def create_func(string):
     matches = re.findall('%\d+', string)
+    curframe = inspect.currentframe()
+    calframe = inspect.getouterframes(curframe, 2)
+    caller_locals = calframe[1][0].f_locals
     if len(matches) == 0:
         def func():
-            return eval(string)
+            return eval(string, {}, caller_locals)
     return func
--- test_anon_func_creator.py
+++ test_anon_func_creator.py
@@ -8,3 +8,9 @@ class TestCreateFunc(unittest.TestCase):
         string = "1+1"
         func = create_func(string)
         self.assertEqual(func(), 2)
+
+    def test_no_args_with_scope(self):
+        x = 10
+        string = "x+1"
+        func = create_func(string)
+        self.assertEqual(func(), 11)
