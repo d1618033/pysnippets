1d4b36db1742929d748acbd0b8fe1c1e998d2f3a
d1618033 <d1618033@gmail.com>
2015-05-26 22:00:55 +0300

    add doc + doctests

--- django_template_renderer.py
+++ django_template_renderer.py
@@ -5,7 +5,25 @@ from django.shortcuts import render
 settings.configure()
 
 def render_template(template_string, context_dictionary):
+    """
+    Renders a django template
+    Input:
+        template_string - str. The template to render.
+        context_dictionary - dict. The dictionary to use to render the template.
+    Output:
+        str. The template string rendered.
+    Example:
+        >>> render_template('{% if x %}{{x}}{% else %}x is falsey{% endif %}', {'x': 5})
+        '5'
+        >>> render_template('{% if x %}{{x}}{% else %}x is falsey{% endif %}', {'x': 0})
+        'x is falsey'
+    """
     engine = Engine()
     template = Template(template_string)
     ctx = Context(context_dictionary)
     return template.render(ctx)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
