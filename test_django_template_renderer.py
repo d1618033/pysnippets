10b8317489ad9a06bebf3cc7e1fb80de79754ad8
d1618033 <d1618033@gmail.com>
2015-05-26 21:55:23 +0300

    add test for for loop

--- test_django_template_renderer.py
+++ test_django_template_renderer.py
@@ -13,4 +13,9 @@ class TestRenderTemplate(unittest.TestCase):
         expected = '1/5/2015'
         actual = render_template('{{a|date:"j/n/Y"}}', {'a': datetime(day=1, month=5, year=2015)})
         self.assertEqual(actual, expected)
-        
+
+    def test_forloop(self):
+        expected = '01234'
+        actual = render_template('{%for x in xs%}{{x}}{%endfor%}', {'xs': range(5)})
+        self.assertEqual(actual, expected)
+
