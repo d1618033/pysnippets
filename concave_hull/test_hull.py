73f618a52aa1fb0a6531a0c76d7f5224c64ec7b6
d1618033 <d1618033@gmail.com>
2015-05-28 05:52:44 +0300

    test ordered using (x,y) vertices

--- concave_hull/test_hull.py
+++ concave_hull/test_hull.py
@@ -4,7 +4,11 @@ from concave_hull import hull
 
 class TestEdgesToVertices(unittest.TestCase):
     def test_ordered(self):
-        edges = [(1, 5), (5, 7), (7, 9)]
-        expected = [1, 5, 7, 9]
+        edges = [
+            [(1, 5), (2, 3)],
+            [(2, 3), (5, 7)],
+            [(5, 7), (7, 9)]
+        ]
+        expected = [(1, 5), (2, 3), (5, 7), (7, 9)]
         actual = hull.edges_to_vertices(edges)
         self.assertEqual(expected, actual)
