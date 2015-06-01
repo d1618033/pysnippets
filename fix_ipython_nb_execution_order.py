374ba1a78fe302d5a2193210f784b73397f15127
d1618033 <d1618033@gmail.com>
2015-06-01 19:34:59 +0300

    fix issue with versions

--- fix_ipython_nb_execution_order.py
+++ fix_ipython_nb_execution_order.py
@@ -1,9 +1,10 @@
-from IPython import nbformat
+from IPython.nbformat import read, write
 import functools
 
 
-def main(file_name):
-    notebook = nbformat.read(file_name, nbformat.NO_CONVERT)
+def main(file_name, input_version=4, output_version=3):
+    with open(file_name) as f:
+        notebook = read(f, input_version)
     cells = notebook['cells']
     executable_cells = filter(lambda cell: 'execution_count' in cell and cell['execution_count'] is not None, cells)
     sorted_cells = sorted(executable_cells, key=lambda cell: cell['execution_count'])
@@ -12,7 +13,8 @@ def main(file_name):
         return dictionary
     correctly_indexed = [setitem(cell, 'execution_count', i+1) for i, cell in enumerate(sorted_cells)]
     notebook['cells'] = correctly_indexed
-    nbformat.write(notebook, file_name)
+    with open(file_name, 'w') as f:
+        write(notebook, f, output_version)
 
 
 if __name__ == "__main__":
